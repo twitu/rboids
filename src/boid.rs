9b71e554f61698a63c3f400a414d72558826ae38
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2020-05-24 01:16:09 +0530

    Add obstacle avoidance

--- src/boid.rs
+++ src/boid.rs
@@ -1,5 +1,7 @@
 use ncollide3d::nalgebra::{Point3, Vector3, Vector4};
 use ncollide3d::nalgebra::geometry::UnitQuaternion;
+use ncollide3d::query::{RayCast, Ray};
+use ncollide3d::math::Isometry;
 use rand::{thread_rng, Rng};
 use rand_distr::{Distribution, UnitSphere};
 
@@ -7,6 +9,32 @@ use rand_distr::{Distribution, UnitSphere};
 const TIME_SCALE: f32 = 1.0;
 const MAX_VEL: f32 = 4.0;
 const MIN_VEL: f32 = 1.0;
+const RAY_NUMS: usize = 100;
+const OBSTACLE_DIST: f32 = 5.0;
+
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref RAY_DIRS: [Vector3<f32>; RAY_NUMS] = {
+        let mut ray_dirs = [Vector3::new(0.0, 0.0, 0.0); RAY_NUMS];
+        // initialize ray angles
+        let golden_ratio: f32 = 1.618;
+        let angle_increment = 3.1415 * 2.0 * golden_ratio;
+
+        for i in 0..RAY_NUMS {
+            let t: f32 = i as f32 / RAY_NUMS as f32;
+            let inclination: f32 = (1.0 - 2.0 * t).acos();
+            let azimuth: f32 = angle_increment * i as f32;
+
+            let x = inclination.sin() * azimuth.cos();
+            let y = inclination.sin() * azimuth.sin();
+            let z = inclination.cos();
+            ray_dirs[i] = Vector3::new(x, y, z);
+        }
+
+        ray_dirs
+    };
+}
 
 /// boid struct is a point with a velocity
 #[derive(Clone)]
@@ -46,44 +74,91 @@ impl Boid {
         self.vel.into()
     }
 
-    pub fn frame_update(&mut self, delta_time: f32) {
-        // update position
-        self.pos += self.vel * delta_time * TIME_SCALE;
-    }
+    /// return unobstructed direction closest to current velocity
+    fn unobstructed_dir(&self, obs: &Vec<(Box<dyn RayCast<f32>>, Isometry<f32>)>) -> Option<Vector3<f32>> {
+        // create a rotation to orient ray directions along velocity
+        let ray_axis: Vector3<f32> = Vector3::new(0.0, 0.0, 1.0);
+        let rot = UnitQuaternion::rotation_between(&ray_axis, &self.vel).unwrap_or(
+            UnitQuaternion::from_axis_angle(&Vector3::x_axis(), std::f32::consts::PI),
+        );
 
-    /// Returns of vector of boids generated randomly inside a given sphere
-    ///
-    /// # Arguments
-    ///
-    /// * `c` - centre of the sphere
-    /// * `r` - radius of the sphere
-    /// * `n` - number of boids to be spawned
-    pub fn spawn_boids(c: &[f32; 3], r: f32, n: usize) -> Vec<Boid> {
-        let centre: Vector3<f32> = Vector3::from_row_slice(c);
-        let mut boids: Vec<Boid> = Vec::new();
-        let mut rng = thread_rng();
-
-        for _ in 0..n {
-            // create position by random offset from centre within given radius
-            let off_value = r * rng.gen_range(-1.0, 1.0);
-            let coords: [f32; 3] = UnitSphere.sample(&mut rng);
-            let offset: Vector3<f32> =
-                Vector3::<f32>::from_row_slice(&coords);
-            let pos: Point3<f32> = Point3::from(centre + offset * off_value);
-
-            // create random velocity with magnitude between MIN_VEL and MAX_VEL
-            let vel_value: f32 = if rng.gen_bool(0.5) {
-                rng.gen_range(MIN_VEL, MAX_VEL)
-            } else {
-                rng.gen_range(-MAX_VEL, -MIN_VEL)
+        // iterate over all ray directions
+        // create a ray from each direction and check for collision
+        let mut best_dir: Option<Vector3<f32>> = None;
+        for dir in RAY_DIRS.iter() {
+            let ray = Ray {
+                origin: self.pos,
+                dir: rot * dir,
             };
-            let coords: [f32; 3] = UnitSphere.sample(&mut rng);
-            let vel: Vector3<f32> = Vector3::<f32>::from_row_slice(&coords) * vel_value;
 
-            // add boid to Vector
-            boids.push(Boid { pos, vel })
+            // if direction is unobstructed store it
+            // after correcting it's orientation
+            if !collided(obs, ray) {
+                best_dir = Some(rot * dir);
+                break;
+            }
         }
 
-        boids
+        // It is possible that there is no unobstructed direction
+        // but that is highly unlikely and possible only when the boid
+        // is entirely surrounded by obstacles.
+        best_dir
     }
+
+    pub fn frame_update(&mut self, obs: &Vec<(Box<dyn RayCast<f32>>, Isometry<f32>)>, delta_time: f32) {
+        // update position
+        self.pos += self.vel * delta_time * TIME_SCALE;
+
+        // check if current heading is obstructed
+        let cur_ray: Ray<f32> = Ray{origin: self.pos, dir: self.vel.normalize()};
+        if collided(obs, cur_ray) {
+            // try to find an unobstructed direction
+            // only affect acceleration if unobstructed direction exists
+            if let Some(dir) = self.unobstructed_dir(obs) {
+                self.vel = dir * self.vel.magnitude();
+            }
+        }
+    }
+}
+
+/// Returns of vector of boids generated randomly inside a given sphere
+///
+/// # Arguments
+///
+/// * `c` - centre of the sphere
+/// * `r` - radius of the sphere
+/// * `n` - number of boids to be spawned
+pub fn spawn_boids(c: &[f32; 3], r: f32, n: usize) -> Vec<Boid> {
+    let centre: Vector3<f32> = Vector3::from_row_slice(c);
+    let mut boids: Vec<Boid> = Vec::new();
+    let mut rng = thread_rng();
+
+    for _ in 0..n {
+        // create position by random offset from centre within given radius
+        let off_value = r * rng.gen_range(-1.0, 1.0);
+        let coords: [f32; 3] = UnitSphere.sample(&mut rng);
+        let offset: Vector3<f32> =
+            Vector3::<f32>::from_row_slice(&coords);
+        let pos: Point3<f32> = Point3::from(centre + offset * off_value);
+
+        // create random velocity with magnitude between MIN_VEL and MAX_VEL
+        let vel_value: f32 = if rng.gen_bool(0.5) {
+            rng.gen_range(MIN_VEL, MAX_VEL)
+        } else {
+            rng.gen_range(-MAX_VEL, -MIN_VEL)
+        };
+        let coords: [f32; 3] = UnitSphere.sample(&mut rng);
+        let vel: Vector3<f32> = Vector3::<f32>::from_row_slice(&coords) * vel_value;
+
+        // add boid to Vector
+        boids.push(Boid { pos, vel })
+    }
+
+    boids
+}
+
+/// check if a ray collides with the given obstacles
+fn collided(obs: &Vec<(Box<dyn RayCast<f32>>, Isometry<f32>)>, ray: Ray<f32>) -> bool {
+    obs.iter()
+        .any(|(shape, iso)| shape.intersects_ray(iso, &ray, OBSTACLE_DIST))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use three;
 use three::{Object, Mesh};
+use ncollide3d::query::RayCast;
+use ncollide3d::math::Isometry;
+use ncollide3d::shape::{Plane, Cylinder};
+use ncollide3d::nalgebra::Vector3;
 
 mod boid;
 use boid::Boid;
@@ -21,15 +25,47 @@ fn main() {
         .up([0.0, 1.0, 0.0])
         .build();
 
-    // add origin
-    let origin = {
-        let geometry = three::Geometry::uv_sphere(2.0, 12, 12);
-        let material = three::material::Wireframe { color: three::color::GREEN };
-        win.factory.mesh(geometry, material)
-    };
-    origin.set_position([0.0, 0.0, 0.0]);
-    win.scene.add(&origin);
+    // add objects to scene
+    add_objects_to_scene(&mut win);
 
+    // create obstacles
+    let obstacles = create_obstacles();
+
+    // create boid
+    let mut boids: Vec<Boid> = boid::spawn_boids(&SPAWN_CENTRE, SPAWN_RADIUS, SPAWN_NUMBER);
+    let cones: Vec<Mesh> = spawn_cones(&mut win);
+
+    // render scene
+    while win.update() && !win.input.hit(three::KEY_ESCAPE) {
+        // update camera transform
+        controls.update(&win.input);
+
+        // compute new boxy velocity and set it
+        boids.iter_mut().for_each(|b: &mut Boid| b.frame_update(&obstacles, win.input.delta_time()));
+        boids.iter().zip(cones.iter()).for_each(|(b, c)| c.set_transform(b.pos_array(), b.rot_array(), 1.0));
+
+        // render scene
+        win.render(&cam);
+    }
+}
+
+fn spawn_cones(win: &mut three::Window) -> Vec<Mesh> {
+    let mut cones: Vec<Mesh> = Vec::new();
+    for _ in 0..SPAWN_NUMBER {
+        let cone = {
+            let geometry = three::Geometry::cylinder(0.0, 1.0, 1.5, 12);
+            let material = three::material::Wireframe { color: three::color::BLACK };
+            win.factory.mesh(geometry, material)
+        };
+        win.scene.add(&cone);
+
+        cones.push(cone);
+    }
+
+    cones
+}
+
+fn add_objects_to_scene(win: &mut three::Window) {
     // add axes
     let x_edge = {
         let geometry = three::Geometry::uv_sphere(0.2, 12, 12);
@@ -55,54 +91,56 @@ fn main() {
     z_edge.set_position([0.0, 0.0, 5.0]);
     win.scene.add(&z_edge);
 
-    // add a solid cone
-    let cone = {
-        let geometry = three::Geometry::cylinder(0.0, 1.0, 1.5, 12);
-        let material = three::material::Basic { color: three::color::BLACK, map: None };
+    let mbox = {
+        let geometry = three::Geometry::cuboid(30.0, 30.0, 30.0);
+        let material = three::material::Wireframe { color: three::color::GREEN };
         win.factory.mesh(geometry, material)
     };
-    cone.set_position([0.0, 0.0, 0.0]);
-    win.scene.add(&cone);
+    mbox.set_position([0.0, 0.0, 0.0]);
+    win.scene.add(&mbox);
 
-    // add a blind spot
-    let blind = {
-        let geometry = three::Geometry::cylinder(0.0, 1.0, 1.5, 12);
-        let material = three::material::Wireframe { color: three::color::RED };
+    let mcone = {
+        let geometry = three::Geometry::cylinder(3.0, 3.0, 30.0, 12);
+        let material = three::material::Wireframe { color: three::color::GREEN };
         win.factory.mesh(geometry, material)
     };
-    blind.set_position([0.0, -1.0, 0.0]);
-    win.scene.add(&blind);
-
-    // create boid
-    let mut boids: Vec<Boid> = Boid::spawn_boids(&SPAWN_CENTRE, SPAWN_RADIUS, SPAWN_NUMBER);
-    let cones: Vec<Mesh> = spawn_cones(&mut win);
-
-    // render scene
-    while win.update() && !win.input.hit(three::KEY_ESCAPE) {
-        // update camera transform
-        controls.update(&win.input);
-
-        // compute new boxy velocity and set it
-        boids.iter_mut().for_each(|b: &mut Boid| b.frame_update(win.input.delta_time()));
-        boids.iter().zip(cones.iter()).for_each(|(b, c)| c.set_transform(b.pos_array(), b.rot_array(), 1.0));
-
-        // render scene
-        win.render(&cam);
-    }
+    mcone.set_position([-10.0, 0.0, 0.0]);
+    win.scene.add(&mcone);
 }
 
-fn spawn_cones(win: &mut three::Window) -> Vec<Mesh> {
-    let mut cones: Vec<Mesh> = Vec::new();
-    for _ in 0..SPAWN_NUMBER {
-        let cone = {
-            let geometry = three::Geometry::cylinder(0.0, 1.0, 1.5, 12);
-            let material = three::material::Wireframe { color: three::color::BLACK };
-            win.factory.mesh(geometry, material)
-        };
-        win.scene.add(&cone);
+/// create vector of obstacles and there corresponding isometric transformations
+/// The obstacle shape is contained in Box to allow for dynamic dispatch
+fn create_obstacles() -> Vec<(Box<dyn RayCast<f32>>, Isometry<f32>)> {
+    // create obstacles
+    let mut obstacles: Vec<(Box<dyn RayCast<f32>>, Isometry<f32>)> = Vec::new();
+    obstacles.push((
+        Box::new(Plane::new(Vector3::x_axis())),
+        Isometry::translation(-15.0, 0.0, 0.0)
+    ));
+    obstacles.push((
+        Box::new(Plane::new(-Vector3::x_axis())),
+        Isometry::translation(15.0, 0.0, 0.0)
+    ));
+    obstacles.push((
+        Box::new(Plane::new(Vector3::y_axis())),
+        Isometry::translation(0.0, -15.0, 0.0)
+    ));
+    obstacles.push((
+        Box::new(Plane::new(-Vector3::y_axis())),
+        Isometry::translation(0.0, 15.0, 0.0)
+    ));
+    obstacles.push((
+        Box::new(Plane::new(Vector3::z_axis())),
+        Isometry::translation(0.0, 0.0, -15.0)
+    ));
+    obstacles.push((
+        Box::new(Plane::new(-Vector3::z_axis())),
+        Isometry::translation(0.0, 0.0, 15.0)
+    ));
+    obstacles.push((
+        Box::new(Cylinder::new(25.0, 3.0)),
+        Isometry::translation(-10.0, 0.0, 0.0)
+    ));
 
-        cones.push(cone);
-    }
-
-    cones
+    obstacles
 }
